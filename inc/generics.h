8bf8325326f9769979e44e144c6ced36d120ee9d
Douglas Ferraz <douglas@nanoleaf.me>
2019-05-27 10:02:10 -0400

    add: LinkedList, Sandbox

---
+++ inc/generics.h
@@ -0,0 +1,12 @@
+#ifndef GENERICS_H
+#define GENERICS_H
+
+/*
+ * This file is used for including all headers from Generics Library
+ */
+
+#include "stack.h"
+#include "misc.h"
+#include "searchArray.h"
+
+#endif /* GENERICS_H */
---
+++ inc/linkedList.h
@@ -0,0 +1,6 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+
+
+#endif /* LINKED_LIST_H */
