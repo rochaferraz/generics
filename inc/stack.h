e785811e5c42ebe6c605314bd02fac4fcfebe3ff
Douglas Rocha Ferraz <rochaferraz@gmail.com>
2019-05-28 22:27:49 -0400

    fix: cleanup

--- inc/stack.h
+++ inc/stack.h
@@ -27,29 +27,45 @@ extern "C" {
 
 	EXAMPLE 1: BASIC USAGE
 
-		SETUPEXAMPLE(sizeof(int));
+ 	stackNew(&s, elementSize, NULL);
+	int numbers[PREALLOC_SIZE*2];
 
-	ASSERT_TRUE(s._allocLen == PREALLOC_SIZE);
-	ASSERT_TRUE(s._logicalLen == 0);
+	for (int i = 0; i < PREALLOC_SIZE*2; i++) {
+		numbers[i] = i;
+	}
+	int* numberVector = numbers;
 
 	for (int i = 0; i < PREALLOC_SIZE; i++) {
 		stackPush(&s, numberVector);
 		numberVector++;
 	}
-	ASSERT_TRUE(s._logicalLen == PREALLOC_SIZE);
-	ASSERT_TRUE(s._allocLen == PREALLOC_SIZE);
-
-	for (int i = 0; i < PREALLOC_SIZE; i++) {
-		stackPush(&s, numberVector);
-		numberVector++;
-	}
-
-	ASSERT_TRUE(s._logicalLen == PREALLOC_SIZE*2);
-	ASSERT_TRUE(s._allocLen == PREALLOC_SIZE*2);
 
 	stackDispose(&s);
 
 	EXAMPLE 2: STRING USAGE -- VARIABLE-LENGTH ELEMENT SIZES
+
+ 	const char* names[] = {"Armando", "Bonilha", "Cercei", "Dmitri", "Elnathan", "Fausto Renato", "Guilherme", "Heitor", "Illich"};
+
+	Stack s;
+	stackNew(&s, sizeof(char*), &stringDispose);
+
+	for (int i = 0; i < PREALLOC_SIZE*2; i++) {
+		char* duplicate = strdup(names[i]);
+		// double indirection
+		stackPush(&s, &duplicate);
+	}
+
+	for (int i = 0; i < PREALLOC_SIZE*2; i++) {
+		char* name;
+		stackPop(&s, &name);
+	}
+
+	for (int i = 0; i < PREALLOC_SIZE; i++) {
+		char* duplicate = strdup(names[i]);
+		stackPush(&s, &duplicate);
+	}
+ 
+	stackDispose(&s);
 
 **/
 
@@ -69,7 +85,7 @@ typedef struct {
 
 // METHODS
 
-void stackNew(Stack *s, int elementSize);
+void stackNew(Stack *s, int elementSize, void (*dealocFunction)(void*));
 void stackDispose(Stack *s);
 void stackPush(Stack *s, void* elementAddress);
 void stackPop(Stack *s, void* elementAddress);
